b46e5d61cdc18dc80b4cf3db3df0099e8e854375
Clement Danso <clement.quoda@gmail.com>
2021-03-07 13:30:55 +0000

    Changes to be committed:

--- webproject/settings.py
+++ webproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,6 @@ os.path.join(BASE_DIR, 'assets'),
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+django_heroku.settings(locals())
